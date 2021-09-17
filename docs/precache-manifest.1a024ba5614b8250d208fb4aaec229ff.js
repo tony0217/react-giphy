d89c90a69298920d426ca0ce499c681863b45994
tony0217 <ahenriquez@bv.studio>
2021-09-17 14:26:10 -0500

    add docs

---
+++ docs/precache-manifest.1a024ba5614b8250d208fb4aaec229ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52d47595a3477f81cde4f5e0d3883f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c04fc8004e9f35dc4477",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04fc8004e9f35dc4477",
+    "url": "/static/js/main.92306e58.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
